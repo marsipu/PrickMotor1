ebba4faa0171c7dfc8681ff5146b0899e440907f
marsipu <dev@earthman-music.de>
2020-12-08 15:07:26 +0100

    Version 9

--- PinprickStimulator1.ino
+++ PinprickStimulator1.ino
@@ -7,24 +7,18 @@
  * D12 - MotorA-Position
  *
  * Trigger-Channels:
- * A0 - Trigger Ch6
- * A2 - Trigger Ch2
- * A3 - Trigger Ch3
- * A4 - Trigger Ch4
- * A5 - Trigger Ch5
- *
- * Outputs to Matlab
- * A1 Start MotorA
- * A2 Start MotorB
- * A1 + A2 End MotorA/End MotorB
+ * A0 - Trigger Ch6 Output
+ * A1 - Trigger Ch1 Output
+ * A2 - Trigger Ch2 Input
+ * A3 - Trigger Ch3 Input
+ * A4 - Trigger Ch4 Output
+ * A5 - Trigger Ch5 Output
  *
  * Trigger #32(6)(A0) - Reserved for Pinprick-Commands, no Command without it
- * Trigger #36(6+3)(A0+A3) - Start Trial
+ * Trigger #36(6+1)(A0+A1) - Start Trial
  * Trigger #40(6+4)(A0+A4) - MotorA goes to Zero-Position
  * Trigger #48(6+5)(A0+A5) - MotorB Reset Position
  *
- * To establish:
- * - Stop command (in delay)
  */
 
 
@@ -37,6 +31,7 @@ const int maxPosition = 9 * stepsPerRevolution; // ca. 0.3cm/Turn, depends on ha
 int Position = 0; // MotorB Position
 int walk = 0;
 int MotorAon = 0;
+int changevelo = 0;
 
 
 //Initialize Motors with Stepper.h-Library
@@ -49,8 +44,9 @@ void setup() {
   pinMode(2, OUTPUT); // enable MotorA
   pinMode(3, OUTPUT); // enable MotorB
   pinMode(A0, INPUT); // Pinprick-Command
+  pinMode(A1, INPUT); // Start Trial
   pinMode(A2, OUTPUT); // Offset-Feedback
-  pinMode(A3, INPUT); // Start Trial
+  pinMode(A3, OUTPUT); // Feedback Velo
   pinMode(A4, INPUT); // MotorA Zero-Position
   pinMode(A5, INPUT); // MotorB Reset Position
 
@@ -70,10 +66,25 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 // Program 1: 1s Trials, Duration:
-if(digitalRead(A0)==1 && digitalRead(A3)==1 && digitalRead(A4)!=1 && digitalRead(A5)!=1){ // #36
+if(digitalRead(A0)==1 && digitalRead(A1)==1 && digitalRead(A4)!=1 && digitalRead(A5)!=1){ // #33
+
+  delay(random(1000));
+
+  if(changevelo==0){
+    MotorA.setSpeed(137);
+    digitalWrite(A2, HIGH);
+    delay(10);
+    digitalWrite(A2, LOW);
+    changevelo=1;
+  }else{
+    MotorA.setSpeed(147);
+    digitalWrite(A3, HIGH);
+    delay(10);
+    digitalWrite(A3, LOW);
+    changevelo=0;
+  }
 
   digitalWrite(2, HIGH); // enable MotorA
-  MotorA.setSpeed(100);
   while(digitalRead(12)==1) MotorA.step(1); // compensate resistance, start at zero
   MotorA.step(stepsPerRevolution); // negative Steps, because the rod lays on the tubes left side
   while(digitalRead(12)==1) MotorA.step(1); // compensate turn resistance, to do exactly one turn
@@ -94,18 +105,36 @@ if(digitalRead(A0)==1 && digitalRead(A3)==1 && digitalRead(A4)!=1 && digitalRead
 }
 
 // Program 2: Offset Response
-if(digitalRead(A0)==1 && digitalRead(A3)==1 && digitalRead(A4)==1 && digitalRead(A5)!=1){ //#44
+if(digitalRead(A0)==1 && digitalRead(A1)==1 && digitalRead(A4)==1 && digitalRead(A5)!=1){ //#41
 
     delay(random(1000));
 
+    if(changevelo==0){
+      MotorA.setSpeed(137);
+      digitalWrite(A2, HIGH);
+      delay(10);
+      digitalWrite(A2, LOW);
+      changevelo=1;
+    }else{
+      MotorA.setSpeed(147);
+      digitalWrite(A3, HIGH);
+      delay(10);
+      digitalWrite(A3, LOW);
+      changevelo=0;
+    }
+
     digitalWrite(2, HIGH); // enable MotorA
-    MotorA.setSpeed(100);
+
     while(digitalRead(12)==1) MotorA.step(1); // compensate resistance, start at zero
     MotorA.step(stepsPerRevolution/2);
+    digitalWrite(2, LOW); // disable MotorA
     delay(2000 + random(1000));
     digitalWrite(A2, HIGH); //Trigger Offset
+    digitalWrite(A3, HIGH);
     delay(10);
     digitalWrite(A2, LOW);
+    digitalWrite(A3, LOW);
+    digitalWrite(2, HIGH); // enable MotorA
     MotorA.step(stepsPerRevolution/2); //Offset Response
     while(digitalRead(12)==1) MotorA.step(1); // compensate turn resistance, to do exactly one turn
     digitalWrite(2, LOW); // disable MotorA
@@ -126,7 +155,7 @@ if(digitalRead(A0)==1 && digitalRead(A3)==1 && digitalRead(A4)==1 && digitalRead
 }
 
 
-if(digitalRead(A0)==1 && digitalRead(A4)==1 && digitalRead(A3)!=1 && digitalRead(A5)!=1 && MotorAon==0){ // get MotorA in Zero Position, Trigger #40
+if(digitalRead(A0)==1 && digitalRead(A4)==1 && digitalRead(A1)!=1 && digitalRead(A5)!=1 && MotorAon==0){ // get MotorA in Zero Position, Trigger #40
   while(digitalRead(12)==1){
     digitalWrite(2, HIGH); // enable MotorA
     MotorA.setSpeed(100);
@@ -136,7 +165,7 @@ if(digitalRead(A0)==1 && digitalRead(A4)==1 && digitalRead(A3)!=1 && digitalRead
 }
 
 
-if(digitalRead(A0)==1 && digitalRead(A4)==1 && digitalRead(A3)!=1 && digitalRead(A5)!=1 && MotorAon==1){ // get MotorA in Zero Position, Trigger #40
+if(digitalRead(A0)==1 && digitalRead(A4)==1 && digitalRead(A1)!=1 && digitalRead(A5)!=1 && MotorAon==1){ // get MotorA in Zero Position, Trigger #40
   MotorAon = 0;
   digitalWrite(2, HIGH); // enable MotorA
   MotorA.step(stepsPerRevolution);
@@ -144,13 +173,30 @@ if(digitalRead(A0)==1 && digitalRead(A4)==1 && digitalRead(A3)!=1 && digitalRead
 }
 
 
-if(digitalRead(A0)==1 && digitalRead(A5)==1 && digitalRead(A3)!=1 && digitalRead(A4)!=1){ // #48
+if(digitalRead(A0)==1 && digitalRead(A5)==1 && digitalRead(A1)!=1 && digitalRead(A4)!=1){ // #48
   Position = 0; // MotorB Reset Position, Trigger #48
-  rounds = 0;
   MotorB.setSpeed(200);
   digitalWrite(3, HIGH); // enable MotorB
   MotorB.step(stepsPerRevolution);
   MotorB.step(-stepsPerRevolution);
+  digitalWrite(3, LOW); // disable MotorB
+}
+
+if(digitalRead(A0)==1 && digitalRead(A5)==1 && digitalRead(A1)==1 && digitalRead(A4)!=1){ // #49
+  Position = 0; // MotorB Reset Position, Trigger #48
+  MotorA.setSpeed(137);
+  digitalWrite(2, HIGH); // enable MotorA
+  MotorA.step(stepsPerRevolution);
+  digitalWrite(2, LOW); // disable MotorA
+  delay(1000);
+  MotorA.setSpeed(147);
+  digitalWrite(2, HIGH); // enable MotorA
+  MotorA.step(stepsPerRevolution);
+  digitalWrite(2, LOW); // disable MotorA
+  delay(1000);
+  MotorB.setSpeed(200);
+  digitalWrite(3, HIGH); // enable MotorB
+  MotorB.step(stepsPerRevolution);
   digitalWrite(3, LOW); // disable MotorB
 }
 
