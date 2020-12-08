9c9876d90807d4b3ec06a72a7be17a5ac003ae0f
marsipu <dev@earthman-music.de>
2020-12-08 15:05:06 +0100

    Version 3

--- PinprickStimulator1.ino
+++ PinprickStimulator1.ino
@@ -1,105 +1,68 @@
+/*
+ * Pinprick-Motor Control
+ * MotorA - Vertical Movement Pinprick
+ * MotorB - Horizontal Movement Stimulator
+ */
 
-#include <Arduino.h>
-#include <ArdumotoStepper.h>
+
+#include <Stepper.h>
 
 const int stepsPerRevolution = 200;
 
-// Motor1(Ardumoto)
-ArdumotoStepper myStepper(stepsPerRevolution);
-
-// Motor2(DRV8825)
-#include "DRV8825.h"
-#define DIR 8
-#define STEP 9
-#define MODE0 10
-#define MODE1 11
-#define MODE2 12
-#define ENABLE 13
-#define MOTOR_STEPS 200
-#define RPM 120
-DRV8825 stepper(MOTOR_STEPS, DIR, STEP, ENABLE, MODE0, MODE1, MODE2);
-
+Stepper MotorA(stepsPerRevolution, 4, 5, 6, 7);
+Stepper MotorB(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(2, INPUT); // On switch
-pinMode(5, INPUT); // Motor1 Starting_Point_Indicator
-pinMode(6, INPUT); // Motor_Test-Switch
-pinMode(8, OUTPUT); // Motor2 Direction
-pinMode(9, OUTPUT); // Motor2 Step
 
-while(digitalRead(5)==1){
-  myStepper.setSpeed(150);
-  myStepper.step(1);
-}
-Serial.begin(9600);
-randomSeed(analogRead(0));
+  pinMode(A2, INPUT); // MotorA Positioning Input
+  pinMode(A3, OUTPUT); // enable MotorA
+  pinMode(A4, OUTPUT); // enable MotorB
 
-stepper.begin(RPM);
-stepper.enable();
+  // get MotorA in Zero Position
+  while(digitalRead(A2)==1){
+    digitalWrite(A3, HIGH); // enable MotorA
+    MotorA.setSpeed(150);
+    MotorA.step(1);
+  }
+  digitalWrite(A3, LOW); // disable MotorA
 
+  Serial.begin(9600);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-
-while(digitalRead(6)){
-  stepper.setMicrostep(1);
-  stepper.rotate(360);     // forward revolution
-  stepper.rotate(-360);    // reverse revolution
-}
-
-//Motor1 Pinprick-Stimulation
-//ER
-if(analogRead(A1)<500){
-
-int ER_Rounds = 1;
-
-
-
-
-    int counter = 0;
-    while(counter<ER_Rounds && digitalRead(2)){
-      myStepper.setSpeed(100);
-      myStepper.step(200);
-      delay(4000);
-      delay(random(1000));
-      counter++;
-
-      if(counter=ER_Rounds-1){
-        myStepper.setSpeed(0);
+if(digitalRead(3)){
+  if(digitalRead(2)){
+    digitalWrite(A3, HIGH); // enable MotorA
+    digitalWrite(A4, LOW); // disable MotorB
+    // read the sensor value:
+    int sensorReading = analogRead(A0);
+    // map it to a range from 0 to 100:
+    int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+    // set the motor speed:
+    if (motorSpeed > 0) {
+      MotorA.setSpeed(motorSpeed);
+      MotorA.step(stepsPerRevolution);
     }
-   }
-
-}
-
-
-
-//Wind-Up
-if(analogRead(A1)>500){
-
-int WU_Rounds = 10;
-
-    int counter = 0;
-    while(counter<WU_Rounds && digitalRead(2)){
-
-      myStepper.setSpeed(100);
-      myStepper.step(200);
-      delay(random(50));
-      delay(random(200));
-      counter++;
-
-
-      if(counter==WU_Rounds-1){
-        int counter = 0;
-        delay(10000);
-      }
+  }else{
+    digitalWrite(A3, LOW); // disable MotorA
+    digitalWrite(A4, HIGH); // enable MotorB
+    // read the sensor value:
+    int sensorReading = analogRead(A0);
+    // map it to a range from 0 to 100:
+    int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+    // set the motor speed:
+    if (motorSpeed > 0) {
+      MotorB.setSpeed(motorSpeed);
+      MotorB.step(stepsPerRevolution);
     }
+  }
 
-
+}else{
+  digitalWrite(A3, LOW);
+  digitalWrite(A4, LOW);
 }
-
-
 }
