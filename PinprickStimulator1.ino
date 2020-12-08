04ab7eac7cc9d3a788a2b04d782cec3fd75b59a1
marsipu <dev@earthman-music.de>
2020-12-08 15:04:40 +0100

    Version 2

--- PinprickStimulator1.ino
+++ PinprickStimulator1.ino
@@ -1,3 +1,5 @@
+
+#include <Arduino.h>
 #include <ArdumotoStepper.h>
 
 const int stepsPerRevolution = 200;
@@ -5,11 +7,26 @@ const int stepsPerRevolution = 200;
 // Motor1(Ardumoto)
 ArdumotoStepper myStepper(stepsPerRevolution);
 
+// Motor2(DRV8825)
+#include "DRV8825.h"
+#define DIR 8
+#define STEP 9
+#define MODE0 10
+#define MODE1 11
+#define MODE2 12
+#define ENABLE 13
+#define MOTOR_STEPS 200
+#define RPM 120
+DRV8825 stepper(MOTOR_STEPS, DIR, STEP, ENABLE, MODE0, MODE1, MODE2);
+
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(2, INPUT); // On switch
 pinMode(5, INPUT); // Motor1 Starting_Point_Indicator
+pinMode(6, INPUT); // Motor_Test-Switch
+pinMode(8, OUTPUT); // Motor2 Direction
+pinMode(9, OUTPUT); // Motor2 Step
 
 while(digitalRead(5)==1){
   myStepper.setSpeed(150);
@@ -18,11 +35,21 @@ while(digitalRead(5)==1){
 Serial.begin(9600);
 randomSeed(analogRead(0));
 
+stepper.begin(RPM);
+stepper.enable();
+
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+while(digitalRead(6)){
+  stepper.setMicrostep(1);
+  stepper.rotate(360);     // forward revolution
+  stepper.rotate(-360);    // reverse revolution
+}
 
 //Motor1 Pinprick-Stimulation
 //ER
@@ -30,8 +57,11 @@ if(analogRead(A1)<500){
 
 int ER_Rounds = 1;
 
+
+
+
     int counter = 0;
-    while(counter<ER_Rounds && digitalRead(2)){  
+    while(counter<ER_Rounds && digitalRead(2)){
       myStepper.setSpeed(100);
       myStepper.step(200);
       delay(4000);
@@ -53,7 +83,7 @@ if(analogRead(A1)>500){
 int WU_Rounds = 10;
 
     int counter = 0;
-    while(counter<WU_Rounds && digitalRead(2)){  
+    while(counter<WU_Rounds && digitalRead(2)){
 
       myStepper.setSpeed(100);
       myStepper.step(200);
@@ -61,7 +91,7 @@ int WU_Rounds = 10;
       delay(random(200));
       counter++;
 
-              
+
       if(counter==WU_Rounds-1){
         int counter = 0;
         delay(10000);
